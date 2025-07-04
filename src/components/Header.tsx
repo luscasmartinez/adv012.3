4fca49aa85db7ac1ce519ca3dcdc4c554b9c1d6a
Lucas de Lima Martinez <106637076+luscasmartinez@users.noreply.github.com>
2025-07-04 13:19:28 -0300

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center">
           <span className="text-[#e5e5e5] font-bold text-xl mx-4 md:text-2xl">
-          DR. Eduardo Lemes Advocacia | OAB/RS nº 136.112 
+          DR. Eduardo Lemes | Advogado | OAB/RS nº 136.112 
           </span>
         </div>
         
