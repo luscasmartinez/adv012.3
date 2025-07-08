34e05dcbe087a03f07ea819a2a9f348d3388b951
Lucas de Lima Martinez <106637076+luscasmartinez@users.noreply.github.com>
2025-07-08 15:44:02 -0300

    removido google calendário

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
       <Header />
       <HeroSection />
       <ProblemSection />
-      <GoogleCalendarScheduling />
       <TestimonialsSection /> 
       <ProcessSection />
       
